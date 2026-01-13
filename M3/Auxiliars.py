ecf4498b48a91e346f5ec5b137d5970b8825ebd4
Aleix Linares Sousa <aleixgls@gmail.com>
2026-01-13 17:08:07 +0100

    explicaciones añadidads

--- M3/Auxiliars.py
+++ M3/Auxiliars.py
@@ -44,10 +44,64 @@ def getHeadeForTableFromTuples(t_name_columns,t_size_columns,title=""):
 
 # HACER
 def getTableFromDict(tuple_of_keys,weigth_of_columns,dict_of_data):
+    # A aquesta funció li passem com a paràmetres, un diccionari del tipus {id: {diccionari amb dades}}
+    # Una tupla amb les keys que ens interessa.
+    # Una tupla amb les grandàries de cada columna.
+    # Per exemple:
+    # tuple_of_keys = (“Username”,”Name”,”CharacterName”,”date”)
+    # weigth_of_columns = (20, 30, 20, 20)
+    # dict_of_data = {
+    #     4: {
+    #         'idUser': 2, 
+    #         'Username': 'Jordi', 
+    #         'idAdventure': 1, 
+    #         'Name': 'Este muerto esta muy vivo',
+    #         'date': datetime.datetime(2021, 11, 28, 18, 17, 20),
+    #         'idCharacter': 1, 
+    #         'CharacterName': 'Beowulf'
+    #     }, 
+    #     5: {
+    #         'idUser': 2, 
+    #         'Username': 'Jordi',
+    #         'idAdventure': 1,
+    #         'Name': 'Este muerto esta muy vivo', 
+    #         'date': datetime.datetime(2021, 11, 26, 13, 28, 36), 
+    #         'idCharacter': 1,
+    #         'CharacterName': 'Beowulf'
+    #     }
+    # }
+    # I ens retorna un string que imprès té forma de taula, amb les dades corresponents a les keys que passem i formatades amb les grandàries donades
     pass
 
 # HACER
 def getOpt(textOpts="",inputOptText="",rangeList=[],dictionary={},exceptions=[]):
+    # Aquesta funció ens prepara un menú en mode text.
+    # El text ens indica les opcions que podem triar després d'indicar-nos què estem escollint una llista o diccionari
+    # Aquest és el text TextOpts que passem.
+    # Pasem també el text que volem veure, a la part on ens indica que escrivim la selecció.
+    # Una llista d'excepcions , que donarà validesa a un valor del menú, per exemple 0 si
+    # volem utilitzar l'opció 0 per a sortir o tornar enrere i no està dins de la nostra llista o
+    # diccionari o qualsevol caràcter especial que vulguem utilitzar com a opció a escollir.
+    # Per exemple:
+    # textOpts = "\n1)Login\n2)Create user\n3)Show Adventures\n4)Exit"
+    # inputOptText = "\nElige tu opción:"
+    # lista = [1,2,3,4]
+    # exceptions = ["w","e",-1]
+    # opc = getOpt(textOpts,inputOptText,lista,exceptions)
+    # Ens mostrarà la sortida:
+    # 1)Login
+    # 2)Create user
+    # 3)Show Adventures
+    # 4)Exit
+    #
+    # Elige tu opción:
+    # I ens retornarà l'opció seleccionada, o mostrarà un missatge d'error si l'opció no és
+    # 1,2,3,4,”w,”,”e” ó -1
+    # El paràmetre rangelist serà la llista d'opcions vàlides del nostre menú (a més a més de
+    # les indicades en excepcions).
+    # Si passem la variable diccionari, les seves claus seran opcions vàlides també.
+
+
     pass
 
 # HACER
@@ -77,4 +131,8 @@ def userExists(user):
 
 # HACER
 def replay(choices):
+    # Aquesta funció serà l'encarregada de fer-nos el replay d'una aventura ja jugada, una vegada hàgim triat el idGame que volem reviure.
+    # Li passarem una tupla de tuples del tipus:
+    # ((pas, selecció), (pas, selecció),(pas, selecció)... )
+    # Amb tots els passos i seleccions que es van fer en aquesta aventura i ens mostrarà l'aventura pas a pas com si l'estiguéssim jugant de nou, però en comptes de demanar-nos triar un pas, ens demanarà que cliquem "Enter" per a continuar.
     pass
